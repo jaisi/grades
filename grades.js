185e9966eb6b0d82b03afdd887ccb94980d9e8ff
Aarti Jaisinghani <jaisina@Aartis-MacBook-Air.local>
2017-04-20 18:03:37 -0500

    min max

--- grades.js
+++ grades.js
@@ -1,4 +1,8 @@
 var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+min = Math.min.apply(Math, scores); 
+max = Math.max.apply(Math, scores);
+console.log("min",min);
+console.log("max", max);
 
 /*
 Loop over an array of student grades (values from 50-100) and output how many of each grades there are.
@@ -10,25 +14,53 @@ A score of 81-90 is a B
 A score of 91-100 is an A
 */
 
+countA = 0;
+countB = 0;
+countC = 0;
+countD = 0;
+countF = 0;
 
 for (i=0; i < scores.length; i++)
 {
-	console.log("your score is ", scores[i]);
+	min = Math.min(scores[i]);
+	//console.log("your score is ", scores[i]);
 	if (scores[i] < 60){
 		grade = "F";
+		countF++;
 		//console.log("your grade is ", grade);
 	} else if (scores[i] > 60 && scores[i] < 71)
 	{
 		grade = "D";
+		countD++;
 	} else if (scores[i] > 70 && scores[i] < 81)
 	{
 		grade = "C";
+		countC++;
 	} else if (scores[i] > 80 && scores[i] < 91)
 	{
 		grade = "B";
-	} else 
+		countB++;
+	}
+	else 
 	{
 		grade = "A";
+		countA++;
 	}
-	console.log("your grade is ", grade); 
+	console.log("Your score is ", scores[i], "Your grade is ", grade);
+	
+
+
 }
+
+var message = "# with A grade:" + countA + ", # with B grade:"+countB+", # with C grade:"+countC+", # with D grade:" +countD+", # with F grade:"+countF;
+console.log(message);
+
+
+
+
+
+
+
+
+
+
